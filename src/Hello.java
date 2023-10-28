ea9bc50326ca3191e3d6cc8304d2df80cd1dfd3f
djechna <damian.jechna@gmail.com>
2023-10-28 11:03:52 +0200

    Second task

--- src/Hello.java
+++ src/Hello.java
@@ -1,5 +1,9 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Imie: Damian");
+        System.out.println("Nazwisko: Jechna");
+        System.out.println("Wiek: 23 lata");
+        System.out.println("Kierunek: Informatyka");
+        System.out.println("Rok: 2");
     }
 }
