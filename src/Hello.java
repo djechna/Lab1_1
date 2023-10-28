c012ecabfc8fc55e2d8b8a124af2fe0d7a369e20
djechna <damian.jechna@gmail.com>
2023-10-28 11:15:34 +0200

    3rd task

--- src/Hello.java
+++ src/Hello.java
@@ -1,9 +1,24 @@
+import java.util.Scanner;
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Imie: Damian");
-        System.out.println("Nazwisko: Jechna");
-        System.out.println("Wiek: 23 lata");
-        System.out.println("Kierunek: Informatyka");
-        System.out.println("Rok: 2");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Podaj imię: ");
+        String imie = scanner.nextLine();
+
+        System.out.print("Podaj nazwisko: ");
+        String nazwisko = scanner.nextLine();
+
+        System.out.print("Podaj wiek (liczbowo): ");
+        String wiek = scanner.nextLine();
+
+        System.out.print("Podaj kierunek studiów: ");
+        String kierunek = scanner.nextLine();
+
+        System.out.print("Podaj rok (liczbowo): ");
+        String rok = scanner.nextLine();
+
+        System.out.println("Imię i nazwisko: "+imie+" "+nazwisko);
+        System.out.println("Wiek: "+wiek+" lat");
+        System.out.println("Kierunek i rok: "+kierunek+", "+rok+" rok");
     }
 }
